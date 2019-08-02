e27b65b3f73ffa32fa65c162549a54af17f947ab
NathanTan <tann@oregonstate.edu>
2019-08-02 16:23:31 -0700

    Quick improvement to forms and added post end poin

--- server.js
+++ server.js
@@ -39,6 +39,24 @@ app.post('/move', (req, res) => {
     res.status(200).sendFile('./draft/move.html', {root: __dirname })
 })
 
+app.post('/pokemon', (req, res) => {
+    console.log(req.body)
+    let pokemon = req.body
+    res.status(200).sendFile('./draft/pokemon.html', {root: __dirname })
+})
+
+app.post('/trainer', (req, res) => {
+    console.log(req.body)
+    let trainer = req.body
+    res.status(200).sendFile('./draft/trainer.html', {root: __dirname })
+})
+
+app.post('/type', (req, res) => {
+    console.log(req.body)
+    let type = req.body
+    res.status(200).sendFile('./draft/type.html', {root: __dirname })
+})
+
 app.use('*', function (req, res, next) {
     res.status(404).json({
         error: "Requested resource " + req.originalUrl + " does not exist"
