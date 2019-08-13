d86e0c69e8ce5a675ccb117d9f38242806ffe3e1
NathanTan <tann@oregonstate.edu>
2019-08-13 10:03:33 -0700

    Fixed up get move by name endpoint.

--- db/move.js
+++ db/move.js
@@ -15,3 +15,34 @@ exports.getAllMoves = function (res, mysqlPool, context, onComplete){
     });
 }
 
+
+// Resulting object defined in data_shapes.json 
+function getMoveByName(db, move_name, res) {
+    let sql = "select * from move where move.name = ?" 
+    let sql2 = "select * from type where id = ?"
+    let someRows, otherRows
+
+    db.query(sql, move_name)
+        .then( rows => {
+            someRows = rows
+            return db.query(sql2, someRows[0].type)
+        })
+        .then( rows => {
+            otherRows = rows
+            return db.close()
+        })
+        .then( () => {
+            let move = {
+                id: someRows[0].id,
+                name: someRows[0].name,
+                power: someRows[0].power,
+                accuracy: someRows[0].accuracy,
+                type: otherRows[0]
+            }
+
+            console.log("Returning move:", move)
+            res.json(move)
+        })
+}
+
+exports.getMoveByName = getMoveByName
